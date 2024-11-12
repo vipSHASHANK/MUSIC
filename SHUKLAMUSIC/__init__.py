555b3ade52db1c0f8d9a039884e59d30c11d8064
˹sᴡᴧɴ ꭙ 𝙲𝚎𝚖𝚊𝚛𝚊⁹⁹˼ 🇮🇩 🇮🇳 <158449266+XFLSWAN@users.noreply.github.com>
2024-11-13 03:42:00 +0530

    Add files via upload

--- SHUKLAMUSIC/__init__.py
+++ SHUKLAMUSIC/__init__.py
@@ -1,1 +1,26 @@
+from SHUKLAMUSIC.core.bot import SHUKLA
+from SHUKLAMUSIC.core.dir import dirr
+from SHUKLAMUSIC.core.git import git
+from SHUKLAMUSIC.core.userbot import Userbot
+from SHUKLAMUSIC.misc import dbb, heroku
 
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = SHUKLA()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
---
+++ SHUKLAMUSIC/__main__.py
@@ -0,0 +1,62 @@
+import asyncio
+import importlib
+
+from pyrogram import idle
+from pytgcalls.exceptions import NoActiveGroupCall
+
+import config
+from SHUKLAMUSIC import LOGGER, app, userbot
+from SHUKLAMUSIC.core.call import SHUKLA
+from SHUKLAMUSIC.misc import sudo
+from SHUKLAMUSIC.plugins import ALL_MODULES
+from SHUKLAMUSIC.utils.database import get_banned_users, get_gbanned
+from config import BANNED_USERS
+
+
+async def init():
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER(__name__).error("Assistant client variables not defined, exiting...")
+        exit()
+    await sudo()
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+    except:
+        pass
+    await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module("SHUKLAMUSIC.plugins" + all_module)
+    LOGGER("SHUKLAMUSIC.plugins").info("Successfully Imported Modules...")
+    await userbot.start()
+    await SHUKLA.start()
+    try:
+        await Sona.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+    except NoActiveGroupCall:
+        LOGGER("SHUKLAMUSIC").error(
+            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
+        )
+        exit()
+    except:
+        pass
+    await Sona.decorators()
+    LOGGER("SHUKLAMUSIC").info(
+        "DROP YOUR GIRLFRIEND'S NUMBER AND SEXY PIC TO @SHIVANSHDEVS , @MASTIWITHFRIENDSXD FOR ANY ISSUES MADE BY SHASHANK SHUKLA"
+    )
+    await idle()
+    await app.stop()
+    await userbot.stop()
+    LOGGER("SHUKLAMUSIC").info("Stopping DEEP Music Bot...")
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(init())
