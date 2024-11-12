de3387da8be95e472d4e9f198f678f4b75435357
˹sᴡᴧɴ ꭙ 𝙲𝚎𝚖𝚊𝚛𝚊⁹⁹˼ 🇮🇩 🇮🇳 <158449266+XFLSWAN@users.noreply.github.com>
2024-11-13 05:08:50 +0530

    Update callback.py

--- SHUKLAMUSIC/plugins/admins/callback.py
+++ SHUKLAMUSIC/plugins/admins/callback.py
@@ -135,7 +135,7 @@ async def del_back_playlist(client, CallbackQuery, _):
             return await CallbackQuery.answer(_["admin_1"], show_alert=True)
         await CallbackQuery.answer()
         await music_off(chat_id)
-        await Sona.pause_stream(chat_id)
+        await SHUKLA.pause_stream(chat_id)
         await CallbackQuery.message.reply_text(
             _["admin_2"].format(mention), reply_markup=close_markup(_)
         )
