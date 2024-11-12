c6f4f59fbca732145965af6114224388a9b473c6
˹sᴡᴧɴ ꭙ 𝙲𝚎𝚖𝚊𝚛𝚊⁹⁹˼ 🇮🇩 🇮🇳 <158449266+XFLSWAN@users.noreply.github.com>
2024-11-13 05:16:38 +0530

    Update admins.py

--- SHUKLAMUSIC/utils/decorators/admins.py
+++ SHUKLAMUSIC/utils/decorators/admins.py
@@ -44,7 +44,7 @@ def AdminRightsCheck(mystic):
                     [
                         InlineKeyboardButton(
                             text="ʜᴏᴡ ᴛᴏ ғɪx ?",
-                            callback_data="SonamousAdmin",
+                            callback_data="SHUKLAmousAdmin",
                         ),
                     ]
                 ]
@@ -140,7 +140,7 @@ def AdminActual(mystic):
                     [
                         InlineKeyboardButton(
                             text="ʜᴏᴡ ᴛᴏ ғɪx ?",
-                            callback_data="SonamousAdmin",
+                            callback_data="SHUKLAmousAdmin",
                         ),
                     ]
                 ]
