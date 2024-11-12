b96b84afbce68ba5913dbe4847e5d54d4a747fc7
˹sᴡᴧɴ ꭙ 𝙲𝚎𝚖𝚊𝚛𝚊⁹⁹˼ 🇮🇩 🇮🇳 <158449266+XFLSWAN@users.noreply.github.com>
2024-11-13 05:14:20 +0530

    Update restart.py

--- SHUKLAMUSIC/plugins/sudo/restart.py
+++ SHUKLAMUSIC/plugins/sudo/restart.py
@@ -18,7 +18,7 @@ from SHUKLAMUSIC.utils.database import (
     remove_active_video_chat,
 )
 from SHUKLAMUSIC.utils.decorators.language import language
-from SHUKLAMUSIC.utils.pastebin import SonaBin
+from SHUKLAMUSIC.utils.pastebin import SHUKLABin
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
