f1eb01f6e7c64ef0bd4f74c59ae0e60377d6693a
˹sᴡᴧɴ ꭙ 𝙲𝚎𝚖𝚊𝚛𝚊⁹⁹˼ 🇮🇩 🇮🇳 <158449266+XFLSWAN@users.noreply.github.com>
2024-11-13 05:02:12 +0530

    Update bot.py

--- SHUKLAMUSIC/core/bot.py
+++ SHUKLAMUSIC/core/bot.py
@@ -6,7 +6,7 @@ import config
 from ..logging import LOGGER
 
 
-class Sona(Client):
+class SHUKLA(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
