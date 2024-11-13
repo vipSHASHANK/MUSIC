69869b47c8b4446ae31e4730ff0411d81e7eafc7
˹sᴡᴧɴ ꭙ 𝙲𝚎𝚖𝚊𝚛𝚊⁹⁹˼ 🇮🇩 🇮🇳 <158449266+XFLSWAN@users.noreply.github.com>
2024-11-13 10:24:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -77,10 +77,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/13afb9ee5c5da17930f1e.png"
+    "START_IMG_URL", "https://files.catbox.moe/nnqkc3.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/13afb9ee5c5da17930f1e.png"
+    "PING_IMG_URL", "https://files.catbox.moe/nnqkc3.jpg"
 )
 PLAYLIST_IMG_URL = "https://telegra.ph/file/13afb9ee5c5da17930f1e.png"
 STATS_IMG_URL = "https://telegra.ph/file/13afb9ee5c5da17930f1e.png"
