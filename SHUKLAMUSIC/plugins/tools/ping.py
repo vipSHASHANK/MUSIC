c8aa15857bbd3f0e4796191dacce6c7f7ee6f76c
XFLSWAN <158449266+XFLSWAN@users.noreply.github.com>
2024-11-18 07:40:33 +0530

    Update ping.py

--- SHUKLAMUSIC/plugins/tools/ping.py
+++ SHUKLAMUSIC/plugins/tools/ping.py
@@ -2,22 +2,22 @@ from datetime import datetime
 
 from pyrogram import filters
 from pyrogram.types import Message
-
-from SHUKLAMUSIC import app
+from config import *
+from ShuklaMUSIC import app
 from SHUKLAMUSIC.core.call import SHUKLA
 from SHUKLAMUSIC.utils import bot_sys_stats
 from SHUKLAMUSIC.utils.decorators.language import language
 from SHUKLAMUSIC.utils.inline import supp_markup
-from config import BANNED_USERS, PING_IMG_URL
+from config import BANNED_USERS
 
 
-@app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
+@app.on_message(filters.command("ping", prefixes=["/"]) & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
     start = datetime.now()
-    response = await message.reply_photo(
-        photo=PING_IMG_URL,
-        caption=_["ping_1"].format(app.mention),
+    # Changed from reply_video to reply
+    response = await message.reply(
+        text=_["ping_1"].format(app.mention),
     )
     pytgping = await SHUKLA.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
